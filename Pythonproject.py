872507d7b21493bc1a796881cb8db951726bf287
Jabez Agyemang-Prempeh <jabez.agyemang-prem@bison.howard.edu>
2022-05-03 01:13:15 +0000

    finished code. About to work on errors

--- Pythonproject.py
+++ Pythonproject.py
@@ -21,7 +21,34 @@ for i in sys.stdin:
                 position = positon + shift - 26
                 empty_str += letters[position]
 
-length = len(empty_str)
-counter = 0
 
 
+ls = [j for i in empty_str for j in i if j!=" "]
+to_print = ""
+length = len(ls)-1
+counter5 = 0
+counter10=0
+change = False
+
+for i in range(length):
+    if counter5 == 5:
+        to_print+=" "
+        #to_print+=i
+        counter5 =1
+        counter10+=1
+        change=True
+    elif counter10 ==10:
+        to_print+="\n"
+        counter10 =1
+        counter5+=1
+        to_print+=ls[i]
+    elif change:
+        to_print+=ls[i-1]
+        to_print+=ls[i]
+        change= False
+        counter5+=1
+    else:
+        to_print+=ls[i]
+        counter5+=1
+
+print(to_print)
